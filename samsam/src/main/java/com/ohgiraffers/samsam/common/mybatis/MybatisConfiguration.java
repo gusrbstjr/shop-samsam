0482289cba78d78ad0d39f85279d8c48f98b89ff
yj0318 <dbswjd0318@gmail.com>
2024-06-21 21:44:29 +0900

    "mapper 부분 추가"

--- samsam/src/main/java/com/ohgiraffers/samsam/common/mybatis/MybatisConfiguration.java
+++ samsam/src/main/java/com/ohgiraffers/samsam/common/mybatis/MybatisConfiguration.java
@@ -1,7 +1,11 @@
 package com.ohgiraffers.samsam.common.mybatis;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@MapperScan(basePackages = "com.ohgiraffers.samsam",annotationClass = Mapper.class)
 public class MybatisConfiguration {
+    // mapperscan 위치 맞는지 확인 부탁드립니다.
 }
