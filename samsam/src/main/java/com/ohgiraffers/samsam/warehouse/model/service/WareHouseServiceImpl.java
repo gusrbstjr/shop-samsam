b75f948469ef8e27fa5938eb914660b302f0ba2d
hyeongyun <gusrbstjr@naver.com>
2024-06-25 09:23:26 +0900

    wareHouse 재고조회 model 생성

--- samsam/src/main/java/com/ohgiraffers/samsam/warehouse/model/service/WareHouseServiceImpl.java
+++ samsam/src/main/java/com/ohgiraffers/samsam/warehouse/model/service/WareHouseServiceImpl.java
@@ -1,4 +1,24 @@
 package com.ohgiraffers.samsam.warehouse.model.service;
 
+import com.ohgiraffers.samsam.warehouse.model.dao.WareHouseMapper;
+import com.ohgiraffers.samsam.warehouse.model.dto.WareHouseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@Slf4j
 public class WareHouseServiceImpl implements WareHouseService{
+
+    private final WareHouseMapper wareHouseMapper;
+
+    public WareHouseServiceImpl(WareHouseMapper wareHouseMapper) {
+        this.wareHouseMapper = wareHouseMapper;
+    }
+
+    @Override
+    public List<WareHouseDTO> AllWareHouse(String WareHouse) {
+        log.info("[WareHouseImpl] AllMenu menuName {}", WareHouse);
+        return wareHouseMapper.AllWareHouse(WareHouse);
+    }
 }
