a46e3107120b20613cf79cb1db7b1fd46e54730b
elliaaa <yunjc536147@gmail.com>
2024-06-26 10:46:20 +0900

    상품 수정및 메인에서 상품관리로 페이지 이동

--- samsam/src/main/java/com/ohgiraffers/samsam/shoppingmall/product/controller/ProductController.java
+++ samsam/src/main/java/com/ohgiraffers/samsam/shoppingmall/product/controller/ProductController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -21,7 +24,7 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping()
+    @GetMapping("/list")
     public String findProductList(Model model, @RequestParam(required = false)String productName) {
 
 
@@ -38,14 +41,38 @@ public class ProductController {
     }
 
     @PostMapping("/regist")
-    public String registProduct(@ModelAttribute ProductDTO newProduct, RedirectAttributes rttr, Model model){
+    public String registProduct(@ModelAttribute ProductDTO newProduct, RedirectAttributes rttr){
 
         System.out.println("newProduct ===============> " + newProduct);
         productService.registNewProduct(newProduct);
 
         rttr.addFlashAttribute("successMessage", "신규 상품 등록에 성공하셨습니다.");
-        return "redirect:/product";
+        return "redirect:/product/list";
     }
 
+
+
+    @GetMapping("/update/{productSeq}")
+    public String updateForm(@PathVariable int productSeq, Model model){
+        ProductDTO productDTO = productService.findByProductSeq(productSeq);
+        System.out.println("productDTO = " + productDTO);
+        model.addAttribute("updateProduct",productDTO);
+
+        return "product/update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute ProductDTO updateProduct, RedirectAttributes rttr){
+
+        System.out.println("updateProduct = " + updateProduct);
+        updateProduct.setRegisterDate(updateProduct.getRegisterDate().formatted("yyyy-MM-dd"));
+        System.out.println("changed value = " + updateProduct);
+        productService.update(updateProduct);
+
+        rttr.addFlashAttribute("successMessage","상품 수정에 성공하셨습니다.");
+
+        return "redirect:/product/list";
+
+    }
 }
 
