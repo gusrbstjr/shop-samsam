20218333fd3e94f006d322686a9410102eeabdc9
yj0318 <dbswjd0318@gmail.com>
2024-06-27 19:07:13 +0900

    "주문 변경 구현했습니다."

--- samsam/src/main/java/com/ohgiraffers/samsam/order/model/dao/OrderChangeMapper.java
+++ samsam/src/main/java/com/ohgiraffers/samsam/order/model/dao/OrderChangeMapper.java
@@ -1,7 +1,16 @@
 package com.ohgiraffers.samsam.order.model.dao;
 
+import com.ohgiraffers.samsam.order.model.dto.OrderChangeDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface OrderChangeMapper {
+    List<OrderChangeDTO> orderChange();
+
+
+    OrderChangeDTO findSequence(int ordersequence);
+
+    int changeStatus(int ordersequence);
 }
