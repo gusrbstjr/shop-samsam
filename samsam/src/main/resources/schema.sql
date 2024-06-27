ab77d233b2dbb7a740305fa40c53756740063646
cylcoder <cylcoder@gmail.com>
2024-06-27 11:32:12 +0900

    schema.sql 수정
    board 테이블의 레코드 값 추가하는 코드 추가

--- samsam/src/main/resources/schema.sql
+++ samsam/src/main/resources/schema.sql
@@ -33,13 +33,13 @@ CREATE TABLE board
         PRIMARY KEY,
     board_title    varchar(255)       NOT NULL,
     board_content  varchar(255)       NOT NULL,
-    board_secrete  int                NOT NULL,
+    board_secrete  int,
     board_date     datetime           NOT NULL,
     customer_seq   int                NOT NULL COMMENT '게시글 혹은 문의글 등록자',
     board_category int                NOT NULL COMMENT '문의글과 공지글 구분',
     answer_content varchar(255)       NULL,
     answer_date    datetime           NULL,
-    answer_status  int DEFAULT 0      NOT NULL COMMENT '문의글의 답변 상태를 표현',
+    answer_status  int DEFAULT 0 COMMENT '문의글의 답변 상태를 표현',
     CONSTRAINT FK_account_TO_board_1
         FOREIGN KEY (customer_seq) REFERENCES account (account_seq)
 );
@@ -184,7 +184,6 @@ VALUES (207, 7, 10, '입고대기', '2024-06-25 16:34:08');
 INSERT INTO samsamdb.warehouse_log (warehouse_seq, product_seq, pro_quantity, status, warehouse_date)
 VALUES (208, 8, 30, '출고완료', '2024-06-25 16:34:20');
 
-
 INSERT INTO samsamdb.account (account_seq, account_id, account_pwd, name, phone_number, mail_addr, account_addr,
                               company_name, business_num, accumulated_amount, subscribe_ads, account_role, grade_seq)
 VALUES (1, 'shopadmin', 'password', '홍길동', '010-1234-5678', 'foo@samsam.com', '서울특별시 강남구', '삼삼안경점', '123-45-67890',
@@ -266,6 +265,7 @@ INSERT INTO samsamdb.account (account_seq, account_id, account_pwd, name, phone_
 VALUES (20, 'shin36', 'uiop3456', '신길동', '010-3333-2222', 'shin@samsam.com', '서울특별시 노원구', '루체안경원', '445-56-77890',
         60000, 0, '고객', 2);
 
+
 INSERT INTO samsamdb.`order` (order_seq, customer_seq, product_seq, total_price, register_date, order_status)
 VALUES (1, 4, 1, 340000, '2024-06-24 18:35:51', '결제완료');
 INSERT INTO samsamdb.`order` (order_seq, customer_seq, product_seq, total_price, register_date, order_status)
@@ -274,3 +274,105 @@ INSERT INTO samsamdb.`order` (order_seq, customer_seq, product_seq, total_price,
 VALUES (3, 5, 6, 160000, '2024-03-24 08:35:51', '발송완료');
 INSERT INTO samsamdb.`order` (order_seq, customer_seq, product_seq, total_price, register_date, order_status)
 VALUES (4, 4, 7, 360000, '2024-01-24 12:55:51', '발송완료');
+
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (5, 'Title 5', 'Content 5', 0, '2024-01-05 12:00:00', 1, 2, 'asd', '2024-06-26 17:29:00', 1);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (6, 'Title 6', 'Content 6', 1, '2024-01-06 12:00:00', 1, 2, 'qwe', '2024-06-27 10:48:22', 1);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (7, 'Title 7', 'Content 7', 0, '2024-01-07 12:00:00', 2, 2, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (8, 'Title 8', 'Content 8', 1, '2024-01-08 12:00:00', 2, 2, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (9, '신제품 교육 세미나 안내', '이번 주 금요일 오후 3시에 신제품 교육 세미나가 있습니다. 모든 직원은 참석해 주시기 바랍니다.', 0, '2024-01-09 12:00:00', 1, 1,
+        NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (11, '여름 휴가 일정 제출 요청', '각 부서별로 여름 휴가 일정을 이번 주말까지 제출해 주세요. 계획에 참고하겠습니다.', 0, '2024-01-11 12:00:00', 1, 1, NULL,
+        NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (12, '매장 리모델링 공사 일정', '다음 주부터 매장 리모델링 공사가 시작됩니다. 고객 안내와 내부 정리에 신경 써 주세요.
+', 1, '2024-01-12 12:00:00', 2, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (13, '정기 재고 조사 안내', '이번 달 말에 정기 재고 조사가 있습니다. 각 부서는 재고 정리를 철저히 해주시기 바랍니다.
+', 0, '2024-01-13 12:00:00', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (14, '신입 직원 오리엔테이션 일정
+
+', '신입 직원 오리엔테이션이 다음 주 월요일 오전 10시에 진행됩니다. 담당자는 준비 부탁드립니다.
+', 1, '2024-01-14 12:00:00', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (15, '고객 불만 처리 절차 개선', '고객 불만 처리 절차가 개선되었습니다. 새로운 절차에 따라 신속히 대응해 주세요.
+', 0, '2024-01-15 12:00:00', 2, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (16, '안전 교육 실시 안내', '직원 안전 교육이 이번 주 수요일 오후 2시에 진행됩니다. 전 직원 참석 필수입니다.
+', 1, '2024-01-16 12:00:00', 2, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (17, '직원 할인 혜택 변경 안내', '직원 할인 혜택이 변경되었습니다. 새로운 혜택 내용을 확인하고 고객에게 안내해 주세요.
+', 0, '2024-01-17 12:00:00', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (18, '업무 평가 회의 일정', '분기별 업무 평가 회의가 다음 주 금요일에 있습니다. 각 부서는 준비해 주시기 바랍니다.
+', 1, '2024-01-18 12:00:00', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (19, '휴무일 변경 안내', '이번 달 휴무일이 변경되었습니다. 공지된 일정 확인 후 업무에 참고해 주세요.
+', 0, '2024-01-19 12:00:00', 2, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (20, '컴퓨터 시스템 점검 공지
+
+', '내일 오전 9시부터 11시까지 컴퓨터 시스템 점검이 있습니다. 점검 시간 동안 업무 조정 부탁드립니다.
+', 1, '2024-01-20 12:00:00', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (22, '청소 및 정리정돈 캠페인', '매장 청결을 위해 청소 및 정리정돈 캠페인을 진행합니다. 모두 적극 참여해 주세요.
+', NULL, '2024-06-26 12:01:57', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (24, '월간 회의 일정 안내', '월간 회의가 매월 첫째 주 월요일 오전 9시에 열립니다. 모든 팀장은 참석해 주세요.
+', NULL, '2024-06-26 12:11:36', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (25, '직원 건강 검진 안내', '직원 건강 검진이 이번 달 말에 있습니다. 건강 검진 일정표를 확인하고 참석해 주세요.
+', NULL, '2024-06-26 15:55:11', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (26, '고객 서비스 개선 아이디어 공모
+
+', '고객 서비스 개선을 위한 아이디어를 공모합니다. 많은 참여 부탁드립니다.
+', NULL, '2024-06-26 15:55:45', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (27, '회사 워크숍 안내', '다음 달 회사 워크숍이 예정되어 있습니다. 세부 일정은 추후 공지하겠습니다.
+', NULL, '2024-06-26 16:03:49', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (28, '주차 공간 이용 안내', '주차 공간이 제한되어 있으니, 가능한 대중교통을 이용해 주세요. 주차 규정을 준수해 주세요.
+', NULL, '2024-06-26 16:07:41', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (29, '팀별 성과 발표회', '이번 주 금요일 오후 4시에 팀별 성과 발표회가 있습니다. 발표 자료 준비 부탁드립니다.
+', NULL, '2024-06-26 16:07:49', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (30, '급여 지급 일정 변경', '이번 달 급여 지급 일정이 변경되었습니다. 급여 명세서를 확인해 주세요.
+', NULL, '2024-06-26 16:11:01', 1, 1, NULL, NULL, 0);
+INSERT INTO samsamdb.board (board_seq, board_title, board_content, board_secrete, board_date, customer_seq,
+                            board_category, answer_content, answer_date, answer_status)
+VALUES (31, 'Title 신규 프로모션 안내
+', '새로운 프로모션이 시작됩니다. 관련 자료를 확인하고 고객에게 안내해 주세요.
+
+', NULL, '2024-06-26 16:12:47', 1, 1, NULL, NULL, 0);
+
